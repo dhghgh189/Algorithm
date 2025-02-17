f79f57a097df60dbc4c8bf2388c22bffdef01e57
dhghgh189 <dhghgh189@gmail.com>
2025-02-17 14:26:48 +0900

    [Feat] 배열의 Swap을 위한 함수 구현

---
+++ Algorithm/Sort/Utils.cs
@@ -0,0 +1,12 @@
+﻿namespace Sort
+{
+    public class Utils
+    {
+        public static void Swap(int[] array, int index1, int index2)
+        {
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+    }
+}
