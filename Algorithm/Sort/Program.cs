07ff15e6bc36aa60e598a9070753995b0cf3fd61
dhghgh189 <dhghgh189@gmail.com>
2025-02-17 14:31:03 +0900

    [Feat] 선택 정렬 구현

--- Algorithm/Sort/Program.cs
+++ Algorithm/Sort/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace Sort
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 3, 9, 5, 7, 6 };
+            int[] array2 = { 6, 7, 5, 3, 1 };
+            int[] array3 = { 8, 1, 6, 2, 5 };
+
+            SelectionSort.Sort(array);
+            SelectionSort.Sort(array2);
+            SelectionSort.Sort(array3);
+
+            foreach (int i in array)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            foreach (int i in array2)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            foreach (int i in array3)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Algorithm/Sort/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Sort
+{
+    public class SelectionSort
+    {
+        public static void Sort(int[] array)
+        {
+            // 최소값을 저장할 변수
+            int minIndex = -1;
+
+            // n-1 회전 수행
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                // 현재 위치를 기준으로 삼는다.
+                minIndex = i;
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    // 나머지 위치들과 비교하면서 최소값을 찾는다.
+                    if (array[j] < array[minIndex])
+                        minIndex = j;
+                }
+
+                // 최소값을 찾으면 교체한다.
+                if (minIndex != i)
+                    Utils.Swap(array, i, minIndex);
+            }
+        }
+    }
+}
