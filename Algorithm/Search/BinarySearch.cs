e856c452afa021dbbb74fc0471ac02237587a551
dhghgh189 <dhghgh189@gmail.com>
2025-02-19 00:00:48 +0900

    [Feat] 이진 탐색 구현

---
+++ Algorithm/Search/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Search
+{
+    public class BinarySearch
+    {
+        public static int Search(int[] array, int target, int low, int high)
+        {
+            // 종료 조건
+            if (low > high)
+                return -1;
+
+            // 중간 값 구하기
+            int mid = (low + high) / 2;
+
+            // target을 찾았을 때
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+            // mid가 target보다 클 때
+            else if (array[mid] > target)
+            {
+                return Search(array, target, low, mid - 1);     // 재귀
+            }
+            // mid가 target보다 작을 때
+            else
+            {
+                return Search(array, target, mid + 1, high);    // 재귀
+            }
+        }
+    }
+}
--- Algorithm/Search/Program.cs
+++ Algorithm/Search/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int[] array = { 2, 11, 97, 34, 55, 34, 21, 69, 52, 9 };
-            int target = 2;
+            int[] array = { 2, 11, 97, 34, 55, 21, 69, 52, 9 };
+            int[] array2 = { 2, 9, 11, 21, 34, 52, 55, 69, 97};
+            int target = 21;
             Console.WriteLine($"Linear Search ({target}) : index {LinearSearch.Search(array, target)}");
+            Console.WriteLine($"Binary Search ({target}) : index {BinarySearch.Search(array2, target, 0, array2.Length-1)}");
         }
     }
 }
