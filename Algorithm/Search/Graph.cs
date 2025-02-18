9c4b6dc1b5a02ed1857de7d41bee2497e1625069
dhghgh189 <dhghgh189@gmail.com>
2025-02-19 01:54:25 +0900

    [Feat] 인접 행렬 그래프 구현

---
+++ Algorithm/Search/Graph.cs
@@ -0,0 +1,32 @@
+﻿namespace Search
+{
+    public class Graph
+    {
+        bool[,] matrix;
+        
+        public Graph(int size)
+        {
+            matrix = new bool[size, size];
+        }
+
+        public void Connect(int start, int end)
+        {
+            matrix[start, end] = true;
+        }
+
+        public void Remove(int start, int end)
+        {
+            matrix[start, end] = false;
+        }
+
+        public bool IsConnected(int start, int end)
+        {
+            return matrix[start, end];
+        }
+
+        public int GetSize()
+        {
+            return matrix.GetLength(0);
+        }
+    }
+}
