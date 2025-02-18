98496a1ee90a94b40e5b2b6de35e8ee1be9955f7
dhghgh189 <dhghgh189@gmail.com>
2025-02-19 01:54:32 +0900

    [Feat] DFS 구현

---
+++ Algorithm/Search/DFS.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+
+namespace Search
+{
+    public class DFS
+    {
+        // 스택 기반 구현
+        public static void Search(Graph graph, int start)
+        {
+            int size = graph.GetSize();
+            int current = -1;
+
+            // 방문한 정점을 기록할 배열
+            bool[] visited = new bool[size];
+
+            // 방문해야 하는 정점을 기록할 스택
+            Stack<int> stack = new Stack<int>(size);
+            stack.Push(start);
+
+            // 스택에 정점이 있는 동안 반복
+            while (stack.Count > 0)
+            {
+                // 현재 방문할 정점을 pop
+                current = stack.Pop();
+
+                // 방문 기록
+                if (!visited[current])
+                {
+                    visited[current] = true;
+                    Console.WriteLine(current);
+                }
+
+                for (int i = 0; i < size; i++)
+                {
+                    // 인접한 정점 중 방문하지 않은 정점을 stack에 저장
+                    if (!visited[i] && graph.IsConnected(current, i))
+                    {
+                        stack.Push(i);
+                    }
+                }
+            }
+        }
+
+        // 재귀 기반 구현
+        public static void Search(Graph graph, int start, out bool[] visited, out int[] parent)
+        {
+            int size = graph.GetSize();
+            // 방문한 정점을 기록할 배열
+            visited = new bool[size];
+            // 각 정점이 어디를 거쳐 방문되었는지 저장할 배열
+            parent = new int[size];
+            // 초기화
+            for (int i = 0; i < size; i++)
+            {
+                parent[i] = -1;
+            }
+            // DFS 시작
+            DFSFunc(graph, start, visited, parent);
+        }
+
+        // 재귀 호출 하는 함수
+        static void DFSFunc(Graph graph, int vertex, bool[] visited, int[] parent)
+        {
+            int size = graph.GetSize();
+            // 방문 기록
+            visited[vertex] = true;
+
+            Console.WriteLine(vertex);
+
+            for (int i = 0; i < size; i++)
+            {
+                // 인접한 정점 중 아직 방문하지 않은 정점을 출발점으로 하여 재귀호출
+                if (!visited[i] && graph.IsConnected(vertex, i))
+                {
+                    parent[i] = vertex;
+                    DFSFunc(graph, i, visited, parent);
+                }
+            }
+        }
+    }
+}
--- Algorithm/Search/Program.cs
+++ Algorithm/Search/Program.cs
@@ -4,11 +4,24 @@
     {
         static void Main(string[] args)
         {
-            int[] array = { 2, 11, 97, 34, 55, 21, 69, 52, 9 };
-            int[] array2 = { 2, 9, 11, 21, 34, 52, 55, 69, 97};
-            int target = 21;
-            Console.WriteLine($"Linear Search ({target}) : index {LinearSearch.Search(array, target)}");
-            Console.WriteLine($"Binary Search ({target}) : index {BinarySearch.Search(array2, target, 0, array2.Length-1)}");
+            //int[] array = { 2, 11, 97, 34, 55, 21, 69, 52, 9 };
+            //int[] array2 = { 2, 9, 11, 21, 34, 52, 55, 69, 97};
+            //int target = 21;
+            //Console.WriteLine($"Linear Search ({target}) : index {LinearSearch.Search(array, target)}");
+            //Console.WriteLine($"Binary Search ({target}) : index {BinarySearch.Search(array2, target, 0, array2.Length-1)}");
+
+            Graph graph = new Graph(7);
+            graph.Connect(0, 1);    graph.Connect(0, 3);
+            graph.Connect(1, 0);    graph.Connect(1, 2);
+            graph.Connect(2, 1);    graph.Connect(2, 3);    graph.Connect(2, 4);    graph.Connect(2, 5);
+            graph.Connect(3, 0);    graph.Connect(3, 2);
+            graph.Connect(4, 2);    graph.Connect(4, 6);
+            graph.Connect(5, 2);
+            graph.Connect(6, 4);
+
+            DFS.Search(graph, 0);
+            Console.WriteLine("=====================");
+            DFS.Search(graph, 0, out bool[] visited, out int[] parent);
         }
     }
 }
