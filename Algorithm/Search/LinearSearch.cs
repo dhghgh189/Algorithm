8eca693a0f130fdedd32051e14d327e0a6e406ce
dhghgh189 <dhghgh189@gmail.com>
2025-02-18 18:04:08 +0900

    [Feat] 선형 탐색 구현

---
+++ Algorithm/Search/LinearSearch.cs
@@ -0,0 +1,19 @@
+﻿namespace Search
+{
+    public class LinearSearch
+    {
+        public static int Search(int[] array, int target)
+        {
+            // 모든 값 순회
+            for (int i = 0; i < array.Length; i++)
+            {
+                // 목표값을 찾으면 index 반환
+                if (array[i] == target)
+                    return i;
+            }
+
+            // 배열에 목표값이 없었던 경우 -1 반환
+            return -1;
+        }
+    }
+}
---
+++ Algorithm/Search/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Search
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 11, 97, 34, 55, 34, 21, 69, 52, 9 };
+            int target = 2;
+            Console.WriteLine($"Linear Search ({target}) : index {LinearSearch.Search(array, target)}");
+        }
+    }
+}
