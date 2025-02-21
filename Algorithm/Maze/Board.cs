c79ac1009df1937a15ea36b5bb85967f3424d8d1
dhghgh189 <dhghgh189@gmail.com>
2025-02-21 15:40:32 +0900

    [Feat] Binary Tree 미로 생성 구현

---
+++ Algorithm/Maze/Board.cs
@@ -0,0 +1,125 @@
+﻿namespace Maze
+{
+    public class Board
+    {
+        const char CIRCLE = '\u25cf';
+
+        // 한칸 한칸의 tile
+        public TileType[,] tiles;
+        public int size;
+
+        public enum TileType { Empty, Wall, }
+
+        public void Init(int size)
+        {
+            // size는 홀수만 가능
+            if (size % 2 == 0)
+                return;
+
+            this.size = size;
+            tiles = new TileType[size, size];
+
+            // Binary Tree를 통한 미로 생성
+            ExecuteBinaryTree();
+        }
+
+        void ExecuteBinaryTree()
+        {
+            // 1. 짝수 칸 타일을 모두 벽으로 만든다.
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    // x 혹은 y축이 짝수인 칸이면
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        // 벽으로 처리
+                        tiles[y, x] = TileType.Wall;
+                    }
+                    else
+                    {
+                        // 짝수가 아니면 비워둔다.
+                        tiles[y, x] = TileType.Empty;
+                    }
+                }
+            }
+
+            // 난수 생성을 위한 Random 인스턴스
+            Random rand = new Random();
+
+            // 2. 홀수 칸을 돌면서 길을 뚫는다.
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    // 짝수칸이면 pass
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        continue;
+                    }
+
+                    // 마지막 위치면 pass
+                    if (x == size - 2 && y == size - 2)
+                        continue;
+
+                    // 현재 순회 위치가 x축 가장자리인 경우
+                    if (x == size - 2)
+                    {
+                        // 반드시 아래로 뚫는다.
+                        tiles[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    // 현재 순회 위치가 y축 가장자리인 경우
+                    if (y == size - 2)
+                    {
+                        // 반드시 우측으로 뚫는다.
+                        tiles[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    // 우측 혹은 아래 중 랜덤으로 길을 뚫는다.
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        tiles[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        tiles[y + 1, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prev = Console.ForegroundColor;
+
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {                  
+                    // 색상 설정
+                    Console.ForegroundColor = GetTileColor(tiles[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prev;
+        }
+
+        public ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ Algorithm/Maze/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Maze
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Init(25);
+
+            // 콘솔에서 커서 출력 감추기
+            Console.CursorVisible = false;
+
+            // 프레임 지정
+            const int FRAME_RATE = 30;
+            // 1000 / FRAME_RATE = 한 프레임당 소요 시간 (ms)
+            const int MS_PER_FRAME = 1000 / FRAME_RATE;
+
+            int lastTick = 0;
+            // Game Loop
+            while (true)
+            {
+                #region 프레임 관리
+                // Environment.TickCount :
+                //      컴퓨터가 마지막으로 시작된 이후 경과된 시간(ms)을 포함하는
+                //      부호 있는 32bit 정수형 (int)
+                // 현재 프레임의 시간을 저장
+                int currentTick = System.Environment.TickCount;
+
+                // 현재시간 - 이전시간 = 경과한 시간
+                // 경과한 시간이 MS_PER_FRAME 보다 작다면 대기
+                if (currentTick - lastTick < MS_PER_FRAME)
+                    continue;
+
+                // 마지막 프레임 진행 시간 저장
+                lastTick = currentTick;
+                #endregion
+
+                // Input
+                // Update (Logic)
+                // Render
+
+                // 커서 위치를 0, 0으로 설정
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
